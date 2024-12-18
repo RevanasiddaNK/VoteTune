e7e4da488705ad60e240531528d1290cdf70b4b5
Revanasidda N Karigoudar <126800532+RevanasiddaNK@users.noreply.github.com>
2024-12-18 23:43:30 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['i.ytimg.com', 'img.lovepik.com', 'img.youtube.com],
+    domains: ['i.ytimg.com', 'img.lovepik.com', 'img.youtube.com'],
   },
   webpack: (config, { isServer }) => {
     // Remove the experimental turbopack configuration
